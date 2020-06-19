1facb3c515dc889532a847169916a88481dd0066
Hanto <jhanto@gmail.com>
2020-06-20 01:06:39 +0200

    First version that works

--- src/main/java/com/myrran/springtest/web/GroupController.java
+++ src/main/java/com/myrran/springtest/web/GroupController.java
@@ -15,47 +15,54 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
-class GroupController {
-
+class GroupController
+{
     private final Logger log = LoggerFactory.getLogger(GroupController.class);
     private GroupRepository groupRepository;
 
-    public GroupController(GroupRepository groupRepository) {
-        this.groupRepository = groupRepository;
-    }
+    public GroupController(GroupRepository groupRepository)
+    {   this.groupRepository = groupRepository; }
 
     @GetMapping("/groups")
     Collection<Group> groups()
-    {
-        return groupRepository.findAll();
-    }
+    {   return groupRepository.findAll(); }
 
     @GetMapping("/group/{id}")
-    ResponseEntity<?> getGroup(@PathVariable Long id) {
+    ResponseEntity<?> getGroup(@PathVariable Long id)
+    {
         Optional<Group> group = groupRepository.findById(id);
-        return group.map(response -> ResponseEntity.ok().body(response))
+        return group
+            .map(response -> ResponseEntity.ok()
+            .body(response))
             .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/group")
-    ResponseEntity<Group> createGroup(@RequestBody Group group) throws URISyntaxException {
+    ResponseEntity<Group> createGroup(@RequestBody Group group) throws URISyntaxException
+    {
         log.info("Request to create group: {}", group);
         Group result = groupRepository.save(group);
-        return ResponseEntity.created(new URI("/api/group/" + result.getId()))
+        return ResponseEntity
+            .created(new URI("/api/group/" + result.getId()))
             .body(result);
     }
 
     @PutMapping("/group/{id}")
-    ResponseEntity<Group> updateGroup(@RequestBody Group group) {
+    ResponseEntity<Group> updateGroup(@RequestBody Group group)
+    {
         log.info("Request to update group: {}", group);
         Group result = groupRepository.save(group);
-        return ResponseEntity.ok().body(result);
+        return ResponseEntity
+            .ok()
+            .body(result);
     }
 
     @DeleteMapping("/group/{id}")
     public ResponseEntity<?> deleteGroup(@PathVariable Long id) {
         log.info("Request to delete group: {}", id);
         groupRepository.deleteById(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity
+            .ok()
+            .build();
     }
 }
